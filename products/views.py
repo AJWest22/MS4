0f8dae12435220f3df663dce6f619f1aedeef4be
AJWest22 <abiwestcott@gmail.com>
2021-12-21 12:59:12 +0000

    updated views.py file

--- products/views.py
+++ products/views.py
@@ -12,6 +12,8 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
         if 'category' in request.GET:
