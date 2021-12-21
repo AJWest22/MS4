2ec73a382d681ea0a0061f43af4b25e8918fa6dd
AJWest22 <abiwestcott@gmail.com>
2021-12-21 13:52:53 +0000

    created basket template and urls.py file

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_basket(request):
+    """ A view that renders the basket's contents page """
+
+    return render(request, 'basket/basket.html')
