cdfb286f6e464762291e50674d13f8c66b48b69e
AJWest22 <abiwestcott@gmail.com>
2021-12-27 11:32:53 +0000

    added save function to override the orginal method

--- checkout/models.py
+++ checkout/models.py
@@ -28,11 +28,11 @@ class Order(models.Model):
         Generates a unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
-
+        
     def update_total(self):
         """
-        Update grand total each time a line item is added,
-        accounting for delivery costs.
+        Update grand total each time an item is added,
+        accounts for delivery if needed.
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
@@ -41,3 +41,15 @@ class Order(models.Model):
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the original save method to set an order number
+        if it hasn't already been set.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
