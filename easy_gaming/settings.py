1f91f76f4f0d7c79d7f777f42641229434c7469e
AJWest22 <abiwestcott@gmail.com>
2021-12-27 14:36:38 +0000

    installed crispy forms in settings.py file

--- easy_gaming/settings.py
+++ easy_gaming/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'checkout',
+
+    #Other installed apps
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'easy_gaming.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 TEMPLATES = [
     {
@@ -76,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'basket.contexts.basket_contents'
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
