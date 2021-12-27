bbfc927d14a00363bc00c646925ddffcae8cacc5
AJWest22 <abiwestcott@gmail.com>
2021-12-27 11:22:18 +0000

    added imports to models.py file in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
