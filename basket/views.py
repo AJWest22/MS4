903b652d4882d93870fdb7d8da7537aeaf0ff63c
AJWest22 <abiwestcott@gmail.com>
2021-12-27 09:10:20 +0000

    added if statement to views.py in bag app

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -14,10 +14,11 @@ def add_to_basket(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
-    else:
-        basket[item_id] = quantity
+    if quantity:
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+        else:
+            basket[item_id] = quantity
 
     request.session['basket'] = basket
     return redirect(redirect_url)
