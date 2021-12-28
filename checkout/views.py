301afe9c6cf2409187dbe2b54a631893560cd22a
AJWest22 <abiwestcott@gmail.com>
2021-12-28 11:10:52 +0000

    added stripe payment method to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
@@ -17,12 +20,22 @@ def checkout(request):
     current_basket = basket_contents(request)
     total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you set it in the environment?')
+
     template = 'checkouts/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KBc3HGvmejGyxf3DkIMPk7rZCLYujJsDKhhzOdzYZYwmKzzhJ3FTBbO1ieI7mjiDRlff70cWy4QlyMZbqrODL0100BMZYqbMF',
+        'stripe_public_key': stripe_public_key,
         'client_secret': 'test client secret',
     }
 
