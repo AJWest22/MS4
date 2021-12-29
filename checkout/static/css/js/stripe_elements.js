c28a9d11067fa892b9c33d386d829520fdeabdc4
AJWest22 <abiwestcott@gmail.com>
2021-12-29 09:22:04 +0000

    updated variable names in js file, and changed text on chekcout.html

--- checkout/static/css/js/stripe_elements.js
+++ checkout/static/css/js/stripe_elements.js
@@ -5,9 +5,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_Public_Key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_Secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
