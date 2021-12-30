5155167bcd03d90fde895814c38a4bfba86be915
AJWest22 <abiwestcott@gmail.com>
2021-12-30 11:22:29 +0000

    updated url and view for profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from .models import UserProfile
+
 
 def profile(request):
     """ Displays a user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
     context = {}
