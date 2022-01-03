68bdb37c455436b031961f6a0499999eff330902
AJWest22 <abiwestcott@gmail.com>
2022-01-03 10:05:04 +0000

    added view to profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -7,7 +7,6 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
-
 def profile(request):
     """ Displays a user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -18,7 +17,7 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
 
-    form = UserProfileForm(request.POST, instance=profile)
+    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -26,6 +25,22 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
