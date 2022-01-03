0962fccaa7b22d3369cdb24db5bcfb81f5ff3d55
AJWest22 <abiwestcott@gmail.com>
2022-01-03 09:51:58 +0000

    imported order to profile views.py

--- profiles/views.py
+++ profiles/views.py
@@ -4,12 +4,13 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 
 def profile(request):
     """ Displays a user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
-
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
@@ -17,7 +18,7 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
 
-    form = UserProfileForm(instance=profile)
+    form = UserProfileForm(request.POST, instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
