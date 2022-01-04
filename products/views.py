7fc24284c065e10ab0f0db730dc530a1cb6978f6
AJWest22 <abiwestcott@gmail.com>
2022-01-04 14:04:06 +0000

    added login required decorator to views.py in products app

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
@@ -70,6 +71,8 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_details.html', context)
 
+
+@login_required
 def add_product(request):
     """ Add a product to the store """
     if request.method == 'POST':
@@ -90,6 +93,8 @@ def add_product(request):
 
     return render(request, template, context)
 
+
+@login_required
 def edit_product(request, product_id):
     """ Edits a product in the store """
     product = get_object_or_404(Product, pk=product_id)
@@ -113,6 +118,8 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
+
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     product = get_object_or_404(Product, pk=product_id)
