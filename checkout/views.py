e9f08d764de8913f7d617da7bcac7f54e24c51a5
AJWest22 <abiwestcott@gmail.com>
2022-01-04 10:09:16 +0000

    added request to views.py and formatting to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -144,25 +144,26 @@ def checkout_complete(request, order_number):
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
     
-    profile = UserProfile.objects.get(user=request.user)
-    # Attaches a user's profile to the order
-    order.user_profile = profile
-    order.save()
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attaches a user's profile to the order
+        order.user_profile = profile
+        order.save()
 
-    # Save the user's info
-    if save_info:
-        profile_data = {
-            'default_phone_number': order.phone_number,
-            'default_country': order.country,
-            'default_postcode': order.postcode,
-            'default_town_or_city': order.town_or_city,
-            'default_street_address1': order.street_address1,
-            'default_street_address2': order.street_address2,
-            'default_county': order.county,
-        }
-        user_profile_form = UserProfileForm(profile_data, instance=profile)
-        if user_profile_form.is_valid():
-            user_profile_form.save()
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
     messages.success(request, f'Order successfully placed! \
         Your order number is {order_number}. A confirmation \
