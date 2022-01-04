f72b20bf871ff7dc8014e78d1e6db19d60bafa05
AJWest22 <abiwestcott@gmail.com>
2022-01-04 12:38:58 +0000

    created forms.py file in products app

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -45,7 +45,7 @@ class StripeWH_Handler:
         """
         intent = event.data.object
         pid = intent.id
-        bag = intent.metadata.bag
+        basket = intent.metadata.basket
         save_info = intent.metadata.save_info
 
         billing_details = intent.charges.data[0].billing_details
