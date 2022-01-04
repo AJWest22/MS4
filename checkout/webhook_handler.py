eeb576fd20fe3618efb6cce62021e8c6cdad4577
AJWest22 <abiwestcott@gmail.com>
2022-01-04 11:18:11 +0000

    added email confirmation to webhook handler.py file

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -13,6 +13,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+    
+    def _send_confirmation_email(self, order):
+        """Sends user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkouts/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkouts/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )        
 
     def handle_event(self, event):
         """
@@ -79,8 +96,9 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order is now in database',
                 status=200)
         else:
             order = None
