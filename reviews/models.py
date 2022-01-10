2e899c10962a327d611da193c14ea7befd2ab145
AJWest22 <abiwestcott@gmail.com>
2022-01-10 09:47:40 +0000

    added reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -2,3 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
+
+class Review(models.Model):
+    """ Review Model """
+    user = models.ForeignKey(
+        User, related_name="reviews", on_delete=models.CASCADE,
+        null=True, blank=True)
+    products = models.ForeignKey(
+        Product, related_name="reviews", on_delete=models.CASCADE)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    title = models.CharField(max_length=255)
+    review = models.TextField(max_length=500)
+
+    def __str__(self):
+        return str(self.title)
