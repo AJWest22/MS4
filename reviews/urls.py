f8dea7b85bdaea62bee8d0f82879397221d742b0
AJWest22 <abiwestcott@gmail.com>
2022-01-24 09:13:37 +0000

    added navigation menu to readme file

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.reviews, name='reviews'),
-    path('add/', views.add_review, name='add_review'),
+    path('reviews/add/', views.add_review),
+    path('reviews/add/<str:search_term>/', views.add_review),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models.functions import Lower
+from .models import Product
 
 from .models import Review
 from .forms import ReviewForm
@@ -21,7 +22,7 @@ def reviews(request):
     return render(request, template, context)
 
 @login_required
-def add_review(request):
+def add_review(request, product_id=None):
     """ Adds reviews """
 
     if request.method == 'POST' and request.user.is_authenticated:
@@ -29,12 +30,17 @@ def add_review(request):
         if form.is_valid():
             review = form.save(commit=False)
             review.user = request.user
+            product = Product.objects.get(pk=product_id)
+            review.product = product
             review.save()
             messages.success(request, "Your review has" +
                              "been submitted for this game")
         else:
-            messages.error(request, 'Sorry, we could not' +
+            messages.error(request, 'Sorry, we couldn''t' +
                            'submit you review right now')
+        if search_term != None:
+        else:
+            return redirect(reverse('reviews'))
     else:
         form = ReviewForm()
 
