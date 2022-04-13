898af8d96095e51c2c40770cacfc15a8a10675ee
AJWest22 <abiwestcott@gmail.com>
2022-04-13 09:24:00 +0000

    add blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    """ Blog Model """
+    user = models.ForeignKey(
+        User, related_name="posts", on_delete=models.CASCADE,
+        null=True, blank=True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    title = models.CharField(max_length=255)
+    review = models.TextField(max_length=500)
+
+    def __str__(self):
+        return str(self.title)
