f8440c512aa5f75e370664a903922588e5f2994a
AJWest22 <abiwestcott@gmail.com>
2022-04-13 12:42:57 +0000

    create ability for superusers to make blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.posts, name='posts'),
+    path('add/', views.add_post, name='add_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -18,3 +18,32 @@ def posts(request):
 
     return render(request, 'blog/blog.html', context)
 
+
+@login_required
+def add_post(request):
+    """ Adds blog posts """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry only site admins can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST' and request.user.is_superuser:
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            post.user = request.user
+            post.save()
+            messages.success(request, "Your post has" +
+                             "been submitted for this blog")
+            return redirect('reviews')
+        else:
+            messages.error(request, 'Sorry, we couldn''t' +
+                           'submit you post right now')    
+    else:
+        form = PostForm()
+
+    template = 'blog/add_blog.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
