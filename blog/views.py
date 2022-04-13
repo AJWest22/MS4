3aaa34684e5b478c42271d247562912b6c5b6cf5
AJWest22 <abiwestcott@gmail.com>
2022-04-13 09:36:51 +0000

    add view to get blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Blog
+from .forms import BlogForm
+
+
+""" Gets all reviews """
+def posts(request):
+
+    posts = Blog.objects.all()
+
+    template = 'blog/blog.html'
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, template, context)
+
