9ca884f34e715475db7d1f6a01473e2c8e98f6d4
AJWest22 <abiwestcott@gmail.com>
2022-05-03 09:34:40 +0000

    add url post_details.html

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.posts, name='posts'),
+    path('<int:post_id>', views.post_details, name='post_details'),
     path('add/', views.add_post, name='add_post'),
     path('edit/<int:post_id>/',
          views.edit_post, name='edit_post'),
