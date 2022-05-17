90d56b1183cd763e5f2eb5c494bd13dc24787b9f
AJWest22 <abiwestcott@gmail.com>
2022-05-17 10:05:48 +0000

    Create newsletter model

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Newsletter(models.Model):
+    """ Newsletter Model """
+    newsletter = models.ForeignKey(
+        User, related_name="newsletter", on_delete=models.CASCADE,
+        null=True, blank=True)
+    Email = models.CharField(max_length=255)
+    Email_confirmation = models.TextField(max_length=250)
+
+    def __str__(self):
+        return str(self.title)
