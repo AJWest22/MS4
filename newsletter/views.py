15326e84b68799711d31832a98222aff2d5b91b2
AJWest22 <abiwestcott@gmail.com>
2022-05-17 14:04:52 +0000

    Update newsletter accessibility

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 
 from .models import Newsletter
 from .forms import NewsletterForm
 
 
-@login_required
 def newsletter(request):
     """ Displays newsletter signup form. """
-    if not request.user.is_authenticated:
-        messages.error(request, 'Sorry only registered users can do that.')
-        return redirect(reverse('home'))
 
-    if request.method == 'POST' and request.user.is_authenticated:
+    if request.method == 'POST':
         form = NewsletterForm(request.POST)
         if form.is_valid():
             newsletter = form.save()
