e631c2b3d47e564e0eb814ad1127c321ea07505f
AJWest22 <abiwestcott@gmail.com>
2022-05-19 06:28:57 +0000

    Correct bug on newsletter form where categories field wouldn't display due to key error

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -14,6 +14,6 @@ class NewsletterForm(forms.ModelForm):
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
         
-        self.fields['category'].choices = friendly_names
+        self.fields['categories'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
