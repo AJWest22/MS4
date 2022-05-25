dcaa8cf45eca491736ac1849511e07a554985649
AJWest22 <abiwestcott@gmail.com>
2022-05-25 12:13:06 +0000

    Update env_sample.py file

--- env_sample.py
+++ env_sample.py
@@ -1,1 +1,12 @@
 import os
+
+os.environ.setdefault('STRIPE_PUBLIC_KEY',)
+os.environ.setdefault('STRIPE_SECRET_KEY',)
+os.environ.setdefault('STRIPE_WH_SECRET',)
+os.environ.setdefault('DATABASE_URL',)
+os.environ.setdefault('AWS_ACCESS_KEY_ID',)
+os.environ.setdefault('AWS_SECRET_ACCESS_KEY',)
+os.environ.setdefault('EMAIL_HOST_PASS',)
+os.environ.setdefault('EMAIL_HOST_USER',)
+os.environ.setdefault('SECRET_KEY',)
+os.environ.setdefault('DEBUG',)
